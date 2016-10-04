d47d8252b4a0b4960b5952ecdff5a3996c9a71c5
Alex Markessinis <markea@sage.edu>
2016-10-04 14:48:30 -0400

    protecting from source dir being destination

--- osBackup.py
+++ osBackup.py
@@ -18,21 +18,26 @@ class osBackup:
             if os.path.isdir(sourceDir):
                 # Ensure that we are not just backing up the root mount, this is bad since we don't need /dev and /tmp and other dirs.
                 if not sourceDir == '/':
+                    # Build the destination path
+                    destination = os.path.join(backupMount, sourceDir)
                     # Make sure we are not syncing the backup mount to the backup location
                     if not sourceDir == backupMount:
-                        # Build the destination path
-                        destination = os.path.join(backupMount, sourceDir)
-                        # Check to make sure that the destination dir exists
-                        if not os.path.isdir(destination):
-                            # If it does not exists make it
-                            print('[INFO] Creating >>> ' + destination)
-                            #mkdirP(destination)
-                        # helpful output
-                        print('[INFO] Syncing ' + sourceDir + ' >>> ' + destination)
-                        # Run our rSync function
-                        #rsync(sourceDir, destination)
+                        if not sourceDir == destination:
+                            # Check to make sure that the destination dir exists
+                            if not os.path.isdir(destination):
+                                # If it does not exists make it
+                                print('[INFO] Creating >>> ' + destination)
+                                # mkdirP(destination)
+                            # helpful output
+                            print('[INFO] Syncing ' + sourceDir + ' >>> ' + destination)
+                            # Run our rSync function
+                            # rsync(sourceDir, destination)
+                        else:
+                            print(
+                                '[WARN] Not syncing... source is the same as destination! That\'s some inception level stuff!')
                     else:
-                        print('[WARN] Not syncing backup disk mount. to backup mount. That\'s some inception level stuff!')
+                        print(
+                            '[WARN] Not syncing backup disk mount. to backup mount. That\'s some inception level stuff!')
                 else:
                     print('[WARN] Not syncing root of filesystem. Please specify more specific directories.')
             else:
